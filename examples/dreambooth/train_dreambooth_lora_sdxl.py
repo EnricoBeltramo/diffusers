53b688a0713260419f795317d535310d91f795b8
Enrico Beltramo <enrico.beltramo@inventio.ai>
2023-08-13 11:06:51 +0200

    removed checkpoint

--- examples/dreambooth/train_dreambooth_lora_sdxl.py
+++ examples/dreambooth/train_dreambooth_lora_sdxl.py
@@ -1296,35 +1296,35 @@ def main(args):
         # `checkpoints_total_limit`
         # EBR added store checkpoint here
 
-        if args.checkpoints_total_limit is not None:
-            checkpoints = os.listdir(args.output_dir)
-            checkpoints = [d for d in checkpoints if d.startswith("checkpoint")]
-            checkpoints = sorted(checkpoints, key=lambda x: int(x.split("-")[1]))
+        # if args.checkpoints_total_limit is not None:
+        #     checkpoints = os.listdir(args.output_dir)
+        #     checkpoints = [d for d in checkpoints if d.startswith("checkpoint")]
+        #     checkpoints = sorted(checkpoints, key=lambda x: int(x.split("-")[1]))
 
-            # before we save the new checkpoint, we need to have at _most_ `checkpoints_total_limit - 1` checkpoints
-            if len(checkpoints) >= args.checkpoints_total_limit:
-                num_to_remove = len(checkpoints) - args.checkpoints_total_limit + 1
-                removing_checkpoints = checkpoints[0:num_to_remove]
+        #     # before we save the new checkpoint, we need to have at _most_ `checkpoints_total_limit - 1` checkpoints
+        #     if len(checkpoints) >= args.checkpoints_total_limit:
+        #         num_to_remove = len(checkpoints) - args.checkpoints_total_limit + 1
+        #         removing_checkpoints = checkpoints[0:num_to_remove]
 
-                logger.info(
-                    f"{len(checkpoints)} checkpoints already exist, removing {len(removing_checkpoints)} checkpoints"
-                )
-                logger.info(f"removing checkpoints: {', '.join(removing_checkpoints)}")
+        #         logger.info(
+        #             f"{len(checkpoints)} checkpoints already exist, removing {len(removing_checkpoints)} checkpoints"
+        #         )
+        #         logger.info(f"removing checkpoints: {', '.join(removing_checkpoints)}")
 
-                for removing_checkpoint in removing_checkpoints:
-                    removing_checkpoint = os.path.join(args.output_dir, removing_checkpoint)
-                    shutil.rmtree(removing_checkpoint)
+        #         for removing_checkpoint in removing_checkpoints:
+        #             removing_checkpoint = os.path.join(args.output_dir, removing_checkpoint)
+        #             shutil.rmtree(removing_checkpoint)
 
-        save_path = os.path.join(args.output_dir, f"checkpoint-{global_step}")
+        # save_path = os.path.join(args.output_dir, f"checkpoint-{global_step}")
 
-        cleanMemory()
-        default_device = accelerator.device
-        acceleratorToDevice(accelerator,'cpu')
-        cleanMemory()
-        accelerator.save_state(save_path)
-        cleanMemory()
+        # cleanMemory()
+        # default_device = accelerator.device
+        # acceleratorToDevice(accelerator,'cpu')
+        # cleanMemory()
+        # accelerator.save_state(save_path)
+        # cleanMemory()
 
-        logger.info(f"Saved state to {save_path}")
+        # logger.info(f"Saved state to {save_path}")
         # EBR added store checkpoint here
 
         unet = accelerator.unwrap_model(unet)
